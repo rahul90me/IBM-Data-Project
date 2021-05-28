7b3403c5fba3ce664fa5c0a59ecb7549de51295d
Rahul kumar <74135831+rahul90me@users.noreply.github.com>
2021-05-28 21:46:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const path = require('path');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const uuid = require('uuid');
+const os = require('os');
+
+require('./config/express')(app);
+
+/**
+ * Parse a base 64 image and return the extension and buffer
+ * @param  {String} imageString The image data as base65 string
+ * @return {Object}             { type: String, data: Buffer }
+ */
+const parseBase64Image = (imageString) => {
+  const matches = imageString.match(/^data:image\/([A-Za-z-+/]+);base64,(.+)$/);
+  const resource = {};
+
+  if (matches.length !== 3) {
+    return null;
+  }
+
+  resource.type = matches[1] === 'jpeg' ? 'jpg' : matches[1];
+  resource.data = new Buffer(matches[2], 'base64');
+  return resource;
+};
+
+let client;
+try {
+  client = new VisualRecognitionV3({
+    // Remember to place the credentials in the .env file. Read the README.md file!
+    version: '2018-03-19',
+  });
+} catch (err) {
+  console.error('Error creating service client: ', err);
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'UP' });
+});
+
+app.post('/api/classify', async (req, res, next) => {
+  let imageFile;
+  let temp
+  if (req.body.image_file) {
+    imageFile = fs.createReadStream(`public/${req.body.image_file}`);
+  }
+  else if (req.body.image_data) {
+    try {
+      const resource = parseBase64Image(req.body.image_data);
+      temp = path.join(os.tmpdir(), `${uuid.v4()}.${resource.type}`);
+      fs.writeFileSync(temp, resource.data);
+      imageFile = fs.createReadStream(temp);
+    } catch (err) {
+      console.error('Error creating image file: ', err);
+      next(err);
+      return;
+    }
+  }
+  const classifyParams = {
+    imagesFile: imageFile,
+    classifierIds: ['default']
+  };
+  try {
+    const response = await client.classify(classifyParams);
+    res.json(response);
+  } catch(err) {
+    console.error(err);
+    if (!client) {
+      const error = {
+        title: 'Invalid credentials',
+        description:
+          'Could not find valid credentials for the Visual Recognition service.',
+        statusCode: 401,
+      };
+      next(error);
+    }
+    next(err);
+  } finally {
+    if (temp) {
+      fs.unlink(temp, (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        
+        //file removed
+      })
+    }
+  }
+});
+
+// error-handler settings for all other routes
+require('./config/error-handler')(app);
+
+module.exports = app;
